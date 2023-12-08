d271f5225cc7e0d51698af32443fb637e4d6f0e5
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-12-07 23:17:34 -0800

    Update schema.js

--- books/schema.js
+++ books/schema.js
@@ -3,11 +3,12 @@ const bookSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
     description: { type: String, required: true },
-    author: { type: String, required: true },
+    author: { type: String },
   },
   {
     collection: "books",
   }
 );
+
 
 export default bookSchema;
